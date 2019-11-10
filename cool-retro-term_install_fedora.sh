097cc578e655655c504ff877ab3ee2986e0e1c1d
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-11-10 09:23:47 -0600

    Update cool-retro-term_install_fedora.sh

--- cool-retro-term_install_fedora.sh
+++ cool-retro-term_install_fedora.sh
@@ -2,8 +2,7 @@ sudo yum -y install qt5-qtbase qt5-qtbase-devel qt5-qtdeclarative qt5-qtdeclarat
 sudo dnf -y install qt5-qtbase qt5-qtbase-devel qt5-qtdeclarative qt5-qtdeclarative-devel qt5-qtgraphicaleffects qt5-qtquickcontrols redhat-rpm-config
 
 
-git clone --recursive ://github.com/Swordfish90/cool-retro-term.git 
-cd cool-retro-term
+
 qmake
 make
 sudo make install
