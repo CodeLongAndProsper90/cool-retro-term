9c28ec457d821d86af733b153371bf01d82e7b0f
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-11-10 09:24:14 -0600

    Update cool-retro-term_install_openSUSE.sh

--- cool-retro-term_install_openSUSE.sh
+++ cool-retro-term_install_openSUSE.sh
@@ -1,7 +1,5 @@
 sudo zypper install libqt5-qtbase-devel libqt5-qtdeclarative-devel libqt5-qtquickcontrols libqt5-qtgraphicaleffects
 
-git clone --recursive https://github.com/Swordfish90/cool-retro-term.git 
-cd cool-retro-term
 qmake
 make
 sudo make install
