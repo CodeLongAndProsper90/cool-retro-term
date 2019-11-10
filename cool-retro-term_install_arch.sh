1f98699fac49857c812fa0f7c53caf3e75e29fff
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-11-10 09:20:56 -0600

    Create cool-retro-term_install_arch.sh

---
+++ cool-retro-term_install_arch.sh
@@ -0,0 +1,9 @@
+sudo pacman -S qt5-base qt5-declarative qt5-quickcontrols qt5-graphicaleffects
+
+
+
+git clone --recursive ://github.com/Swordfish90/cool-retro-term.git 
+cd cool-retro-term
+qmake
+make
+sudo make install
