cf52aea5a0a9bb303318f01ea5975f079a97aee4
CodeLongAndProsper90 <50145141+CodeLongAndProsper90@users.noreply.github.com>
2019-11-10 09:25:04 -0600

    Update cool-retro-term_install_ubuntu16.sh

--- cool-retro-term_install_ubuntu16.sh
+++ cool-retro-term_install_ubuntu16.sh
@@ -1,7 +1,6 @@
 sudo apt-get install build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev qml-module-qtquick-controls qtdeclarative5-qtquick2-plugin libqt5qml-graphicaleffects qml-module-qtquick-dialogs qtdeclarative5-localstorage-plugin qtdeclarative5-window-plugin
 
-git clone --recursive ://github.com/Swordfish90/cool-retro-term.git 
-cd cool-retro-term
+
 qmake
 make
 sudo make install
